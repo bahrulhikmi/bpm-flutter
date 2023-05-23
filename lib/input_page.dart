4c5b7acbe795f59136e5224c8b6ed965ca1bc9a1
bahrul.hikmi <bahrul.hikmi@unit4.com>
2023-05-23 16:57:20 +0700

    Update input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -84,17 +84,26 @@ class _InputPageState extends State<InputPage> {
                       )
                     ],
                   ),
-                  Slider(
-                      activeColor: Color(0xFFEB1555),
-                      inactiveColor: Color(0xFF8D8E98),
-                      value: height.toDouble(),
-                      min: 100.0,
-                      max: 220.0,
-                      onChanged: (double){
-                        setState(() {
-                          height = double.toInt();
-                        });
-                      })
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                        activeTrackColor: Colors.white,
+                        thumbColor: Color(0xFFEB1555),
+                        overlayColor: Color(0x29EB1555),
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0)),
+                    child: Slider(
+                        value: height.toDouble(),
+                        min: 100.0,
+                        max: 220.0,
+                        onChanged: (double) {
+                          setState(() {
+                            height = double.toInt();
+                          });
+                        }),
+                  )
                 ],
               ),
             )),
